4f14bbea7f1d0c5a6bc28cda54ada7a2bdefa381
yoshizawa-katsuya <k023g0036@g.neec.ac.jp>
2024-03-09 11:17:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Matrix.cpp
@@ -0,0 +1,153 @@
+#include "Matrix.h"
+
+Matrix3x3 MakeTranslateMatrix(Vector2 translate) {
+	Matrix3x3 matrix;
+
+	matrix.m[0][0] = 1;
+	matrix.m[0][1] = 0;
+	matrix.m[0][2] = 0;
+	matrix.m[1][0] = 0;
+	matrix.m[1][1] = 1;
+	matrix.m[1][2] = 0;
+	matrix.m[2][0] = translate.x;
+	matrix.m[2][1] = translate.y;
+	matrix.m[2][2] = 1;
+
+	return matrix;
+}
+
+Matrix3x3 Invarse(Matrix3x3 matrix) {
+	Matrix3x3 invarse;
+	float A_1 = 1 / (matrix.m[0][0] * matrix.m[1][1] * matrix.m[2][2] + matrix.m[0][1] * matrix.m[1][2] * matrix.m[2][0] + matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][1]
+		- matrix.m[0][2] * matrix.m[1][1] * matrix.m[2][0] - matrix.m[0][1] * matrix.m[1][0] * matrix.m[2][2] - matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][1]);
+
+	invarse.m[0][0] = A_1 * (matrix.m[1][1] * matrix.m[2][2] - matrix.m[1][2] * matrix.m[2][1]);
+	invarse.m[0][1] = A_1 * -(matrix.m[0][1] * matrix.m[2][2] - matrix.m[0][2] * matrix.m[2][1]);
+	invarse.m[0][2] = A_1 * (matrix.m[0][1] * matrix.m[1][2] - matrix.m[0][2] * matrix.m[1][1]);
+	invarse.m[1][0] = A_1 * -(matrix.m[1][0] * matrix.m[2][2] - matrix.m[1][2] * matrix.m[2][0]);
+	invarse.m[1][1] = A_1 * (matrix.m[0][0] * matrix.m[2][2] - matrix.m[0][2] * matrix.m[2][0]);
+	invarse.m[1][2] = A_1 * -(matrix.m[0][0] * matrix.m[1][2] - matrix.m[0][2] * matrix.m[1][0]);
+	invarse.m[2][0] = A_1 * (matrix.m[1][0] * matrix.m[2][1] - matrix.m[1][1] * matrix.m[2][0]);
+	invarse.m[2][1] = A_1 * -(matrix.m[0][0] * matrix.m[2][1] - matrix.m[0][1] * matrix.m[2][0]);
+	invarse.m[2][2] = A_1 * (matrix.m[0][0] * matrix.m[1][1] - matrix.m[0][1] * matrix.m[1][0]);
+
+	return invarse;
+}
+
+Matrix3x3 MakeOrthographicMatrix(float left, float top, float right, float bottom) {
+	Matrix3x3 matrix;
+
+	matrix.m[0][0] = 2 / (right - left);
+	matrix.m[0][1] = 0;
+	matrix.m[0][2] = 0;
+	matrix.m[1][0] = 0;
+	matrix.m[1][1] = 2 / (top - bottom);
+	matrix.m[1][2] = 0;
+	matrix.m[2][0] = (left + right) / (left - right);
+	matrix.m[2][1] = (top + bottom) / (bottom - top);
+	matrix.m[2][2] = 1;
+
+	return matrix;
+}
+
+Matrix3x3 MakeViewportMatrix(float left, float top, float wigth, float height) {
+	Matrix3x3 matrix;
+
+	matrix.m[0][0] = wigth / 2;
+	matrix.m[0][1] = 0;
+	matrix.m[0][2] = 0;
+	matrix.m[1][0] = 0;
+	matrix.m[1][1] = -(height / 2);
+	matrix.m[1][2] = 0;
+	matrix.m[2][0] = left + (wigth / 2);
+	matrix.m[2][1] = top + (height / 2);
+	matrix.m[2][2] = 1;
+
+	return matrix;
+
+
+}
+
+Matrix3x3 Multiply(Matrix3x3 matrix1, Matrix3x3 matrix2) {
+	Matrix3x3 matrix;
+
+	matrix.m[0][0] = matrix1.m[0][0] * matrix2.m[0][0] + matrix1.m[0][1] * matrix2.m[1][0] + matrix1.m[0][2] * matrix2.m[2][0];
+	matrix.m[0][1] = matrix1.m[0][0] * matrix2.m[0][1] + matrix1.m[0][1] * matrix2.m[1][1] + matrix1.m[0][2] * matrix2.m[2][1];
+	matrix.m[0][2] = matrix1.m[0][0] * matrix2.m[0][2] + matrix1.m[0][1] * matrix2.m[1][2] + matrix1.m[0][2] * matrix2.m[2][2];
+	matrix.m[1][0] = matrix1.m[1][0] * matrix2.m[0][0] + matrix1.m[1][1] * matrix2.m[1][0] + matrix1.m[1][2] * matrix2.m[2][0];
+	matrix.m[1][1] = matrix1.m[1][0] * matrix2.m[0][1] + matrix1.m[1][1] * matrix2.m[1][1] + matrix1.m[1][2] * matrix2.m[2][1];
+	matrix.m[1][2] = matrix1.m[1][0] * matrix2.m[0][2] + matrix1.m[1][1] * matrix2.m[1][2] + matrix1.m[1][2] * matrix2.m[2][2];
+	matrix.m[2][0] = matrix1.m[2][0] * matrix2.m[0][0] + matrix1.m[2][1] * matrix2.m[1][0] + matrix1.m[2][2] * matrix2.m[2][0];
+	matrix.m[2][1] = matrix1.m[2][0] * matrix2.m[0][1] + matrix1.m[2][1] * matrix2.m[1][1] + matrix1.m[2][2] * matrix2.m[2][1];
+	matrix.m[2][2] = matrix1.m[2][0] * matrix2.m[0][2] + matrix1.m[2][1] * matrix2.m[1][2] + matrix1.m[2][2] * matrix2.m[2][2];
+
+	return matrix;
+}
+
+Vector2 Transform(Vector2 vector, Matrix3x3 matrix) {
+	Vector2 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + 1.0f * matrix.m[2][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + 1.0f * matrix.m[2][1];
+	float w = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + 1.0f * matrix.m[2][2];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+
+	return result;
+}
+
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
+
+	Matrix4x4 matrix;
+
+	matrix.m[0][0] = m1.m[0][0] + m2.m[0][0];
+	matrix.m[0][1] = m1.m[0][1] + m2.m[0][1];
+	matrix.m[0][2] = m1.m[0][2] + m2.m[0][2];
+	matrix.m[0][3] = m1.m[0][3] + m2.m[0][3];
+
+	matrix.m[1][0] = m1.m[1][0] + m2.m[1][0];
+	matrix.m[1][1] = m1.m[1][1] + m2.m[1][1];
+	matrix.m[1][2] = m1.m[1][2] + m2.m[1][2];
+	matrix.m[1][3] = m1.m[1][3] + m2.m[1][3];
+
+	matrix.m[2][0] = m1.m[2][0] + m2.m[2][0];
+	matrix.m[2][1] = m1.m[2][1] + m2.m[2][1];
+	matrix.m[2][2] = m1.m[2][2] + m2.m[2][2];
+	matrix.m[2][3] = m1.m[2][3] + m2.m[2][3];
+
+	matrix.m[3][0] = m1.m[3][0] + m2.m[3][0];
+	matrix.m[3][1] = m1.m[3][1] + m2.m[3][1];
+	matrix.m[3][2] = m1.m[3][2] + m2.m[3][2];
+	matrix.m[3][3] = m1.m[3][3] + m2.m[3][3];
+
+	return matrix;
+
+}
+
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
+
+	Matrix4x4 matrix;
+
+	matrix.m[0][0] = m1.m[0][0] - m2.m[0][0];
+	matrix.m[0][1] = m1.m[0][1] - m2.m[0][1];
+	matrix.m[0][2] = m1.m[0][2] - m2.m[0][2];
+	matrix.m[0][3] = m1.m[0][3] - m2.m[0][3];
+
+	matrix.m[1][0] = m1.m[1][0] - m2.m[1][0];
+	matrix.m[1][1] = m1.m[1][1] - m2.m[1][1];
+	matrix.m[1][2] = m1.m[1][2] - m2.m[1][2];
+	matrix.m[1][3] = m1.m[1][3] - m2.m[1][3];
+
+	matrix.m[2][0] = m1.m[2][0] - m2.m[2][0];
+	matrix.m[2][1] = m1.m[2][1] - m2.m[2][1];
+	matrix.m[2][2] = m1.m[2][2] - m2.m[2][2];
+	matrix.m[2][3] = m1.m[2][3] - m2.m[2][3];
+
+	matrix.m[3][0] = m1.m[3][0] - m2.m[3][0];
+	matrix.m[3][1] = m1.m[3][1] - m2.m[3][1];
+	matrix.m[3][2] = m1.m[3][2] - m2.m[3][2];
+	matrix.m[3][3] = m1.m[3][3] - m2.m[3][3];
+
+	return matrix;
+
+}
