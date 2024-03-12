a9490478fee0c65952420ee5c463e25c56ceca21
yoshizawa-katsuya <k023g0036@g.neec.ac.jp>
2024-03-12 09:00:45 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <Novice.h>
+#include "Matrix.h"
 
 const char kWindowTitle[] = "LC1A_27_ヨシザワ_カツヤ";
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +22,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Matrix4x4 m1 = { 3.2f, 0.7f, 9.6f, 4.4f,
+					 5.5f, 1.3f, 7.8f, 2.1f,
+					 6.9f, 8.0f, 2.6f, 1.0f,
+					 0.5f, 7.2f, 5.1f, 3.3f };
+
+	Matrix4x4 m2 = { 4.1f, 6.5f, 3.3f, 2.2f,
+					 8.8f, 0.6f, 9.9f, 7.7f,
+					 1.1f, 5.5f, 6.6f, 0.0f,
+					 3.3f, 9.9f, 8.8f, 2.2f };
+
+	Matrix4x4 resultAdd = Add(m1, m2);
+	Matrix4x4 resultMultiply = Multiply(m1, m2);
+	Matrix4x4 resultSubtract = Subtract(m1, m2);
+	Matrix4x4 inverseM1 = Inverse(m1);
+	Matrix4x4 inverseM2 = Inverse(m2);
+	Matrix4x4 transposeM1 = Transpose(m1);
+	Matrix4x4 transposeM2 = Transpose(m2);
+	Matrix4x4 identity = MakeIdentity4x4();
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -32,6 +62,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, resultAdd);
+		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, inverseM1);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 4, inverseM2);
+		MatrixScreenPrintf(kColumnWidth * 5, 0, transposeM1);
+		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, transposeM2);
+		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5 * 2, identity);
 
 		///
 		/// ↑描画処理ここまで
